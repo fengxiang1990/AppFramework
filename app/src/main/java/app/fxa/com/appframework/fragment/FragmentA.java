e38d44062a9b50fbff0167a87df840d0bcfa07f0
303153041@qq.com <fengxiang1990529>
2015-12-29 18:30:55 +0800

    集成toolbar

---
+++ app/src/main/java/app/fxa/com/appframework/fragment/FragmentA.java
@@ -0,0 +1,20 @@
+package app.fxa.com.appframework.fragment;
+
+import android.support.v4.app.Fragment;
+import android.widget.TextView;
+
+import org.androidannotations.annotations.EFragment;
+import org.androidannotations.annotations.ViewById;
+
+import app.fxa.com.appframework.BaseFragment;
+import app.fxa.com.appframework.R;
+
+/**
+ * Created by admin on 2015/12/28.
+ */
+
+@EFragment(R.layout.fragment_a)
+public class FragmentA extends BaseFragment {
+
+
+}
